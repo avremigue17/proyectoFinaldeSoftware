eaeff2c2567b7e8ceebf3ab730d38b05685122c1
avremigue17 <62452007+avremigue17@users.noreply.github.com>
2021-03-24 20:04:06 -0700

    Revert "header"
    This reverts commit bd6099499ebd0e11556fe386a591b457fb25614e.

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -1,151 +1,123 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" style="background-color: rgb(26,32,44);">
         <div class="flex justify-between h-16">
-            <div class="flex" style="width: 30%">
+            <div class="flex">
                 <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center" style="background-color: white">
+                <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="{{url('/')}}/img/logo.png" class="card-img-top" alt="..." style="width: 120px; height: 70px;margin-left: 50%">
+                        <img src="{{url('/')}}/img/logo.png" class="card-img-top" alt="..." style="width: 120px; height: 100px; margin-top: 10px">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                
-            </div>
-            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex" style="width: 40%;">
-                    <nav class="navbar navbar-light bg">
-                      <div class="container-fluid">
-                        <form class="d-flex">
-                          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                          <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                      </div>
-                    </nav>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" style="color: white;font-size: 20px">
+                        {{ __('Inicio') }}
+                    </x-jet-nav-link>
                 </div>
+            </div>
 
             <!-- Settings Dropdown -->
-           
-            <div class="hidden sm:flex sm:items-center sm:ml-6" style="width: 30%;">
-                <div style="width: 70%">
-                    <div style="float: right;" >
-                        <a class="navbar-brand" href="#">
-                        <img src="{{url('/')}}/img/gps.png" class="card-img-top" alt="..." style="width: 20px; height: 20px; margin-top: 50%">
-                        </a>
-                    </div>
-                    <div style="float: right;" >
-                        <a class="navbar-brand" href="#">
-                        <img src="{{url('/')}}/img/cora.png" class="card-img-top" alt="..." style="width: 20px; height: 20px; margin-top: 50%">
-                        </a>
-                    </div>
-                    <div style="float: right;" >
-                        <a class="navbar-brand" href="#">
-                        <img src="{{url('/')}}/img/messenger.png" class="card-img-top" alt="..." style="width: 20px; height: 20px; margin-top: 50%">
-                        </a>
-                    </div>
-                    <div style="float: right;"  >
-                        <a class="navbar-brand" href="#">
-                        <img src="{{url('/')}}/img/mas.png" class="card-img-top" alt="..." style="width: 20px; height: 20px; margin-top: 50%">
-                        </a>
-                    </div>
-                    <div style="float: right;" >
-                        <a class="navbar-brand" href="#">
-                        <img src="{{url('/')}}/img/home.png" class="card-img-top" alt="..." style="width: 20px; height: 20px; margin-top: 50%">
-                        </a>
-                    </div>     
-                </div> 
-                <div style="float: left;" >
-                    <x-jet-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <x-jet-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
                                 <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                </button>
-                            @else
-                                <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                    <div style="float: right;" >
-                                        <a class="navbar-brand" href="#">
-                                        <img src="{{url('/')}}/img/user.png" class="card-img-top" alt="..." style="width: 23px; height: 23px; margin-top: 50%; float: right;">
-                                        </a>
-                                    </div>
-                                </button>
-                            @endif
-                        </x-slot>
-                        <x-slot name="content">
-                            <!-- Account Management -->
+                            </button>
+                        @else
+                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <div style="color: white"><p style="font-weight: bolder;font-size: 20px; margin: 0">Menu</p><p style="margin: 0">{{ Auth::user()->name }}</p></div>
+
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        @endif
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <!-- Account Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('Opciones') }}
+                        </div>
+
+                        <x-jet-responsive-nav-link  href="{{ route('profile.show') }}">
+                            {{ __('ya veremos') }}
+                        </x-jet-responsive-nav-link>
+                        
+                        <x-jet-responsive-nav-link href="{{ route('movies')}}">
+                            {{ __('Perfil') }}
+                        </x-jet-responsive-nav-link>
+
+                         <x-jet-responsive-nav-link href="{{ route('loans')}}">
+                            {{ __('Prestamos') }}
+                        </x-jet-responsive-nav-link>
+
+                        @if(Auth::user()->hasRole('Admin'))
+                        <x-jet-responsive-nav-link href="{{ route('categories')}}">
+                            {{ __('Categorias') }}
+                        </x-jet-responsive-nav-link>
+
+                        <x-jet-responsive-nav-link href="{{ route('users')}}">
+                            {{ __('Usuarios') }}
+                        </x-jet-responsive-nav-link>
+                        @endif
+
+                        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                            <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
+                                {{ __('API Tokens') }}
+                            </x-jet-dropdown-link>
+                        @endif
+
+                        <div class="border-t border-gray-100"></div>
+
+                        <!-- Team Management -->
+                        @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                             <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Opciones') }}
+                                {{ __('Manage Team') }}
                             </div>
-                            <x-jet-responsive-nav-link  href="{{ route('profile.show') }}">
-                                {{ __('Perfil') }}
-                            </x-jet-responsive-nav-link>
-                            
-                            <x-jet-responsive-nav-link href="{{ route('movies')}}">
-                                {{ __('Guardado') }}
-                            </x-jet-responsive-nav-link>
 
-                            @if(Auth::user()->hasRole('Admin'))
-                            <x-jet-responsive-nav-link href="{{ route('categories')}}">
-                                {{ __('Categorias') }}
-                            </x-jet-responsive-nav-link>
+                            <!-- Team Settings -->
+                            <x-jet-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
+                                {{ __('Team Settings') }}
+                            </x-jet-dropdown-link>
 
-                            <x-jet-responsive-nav-link href="{{ route('users')}}">
-                                {{ __('Usuarios') }}
-                            </x-jet-responsive-nav-link>
-                            @endif
-
-                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
-                                    {{ __('API Tokens') }}
+                            @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
+                                <x-jet-dropdown-link href="{{ route('teams.create') }}">
+                                    {{ __('Create New Team') }}
                                 </x-jet-dropdown-link>
-                            @endif
+                            @endcan
 
                             <div class="border-t border-gray-100"></div>
 
-                            <!-- Team Management -->
-                            @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
-                                <div class="block px-4 py-2 text-xs text-gray-400">
-                                    {{ __('Manage Team') }}
-                                </div>
+                            <!-- Team Switcher -->
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                {{ __('Switch Teams') }}
+                            </div>
 
-                                <!-- Team Settings -->
-                                <x-jet-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
-                                    {{ __('Team Settings') }}
-                                </x-jet-dropdown-link>
+                            @foreach (Auth::user()->allTeams() as $team)
+                                <x-jet-switchable-team :team="$team" />
+                            @endforeach
 
-                                @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-                                    <x-jet-dropdown-link href="{{ route('teams.create') }}">
-                                        {{ __('Create New Team') }}
-                                    </x-jet-dropdown-link>
-                                @endcan
+                            <div class="border-t border-gray-100"></div>
+                        @endif
 
-                                <div class="border-t border-gray-100"></div>
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
 
-                                <!-- Team Switcher -->
-                                <div class="block px-4 py-2 text-xs text-gray-400">
-                                    {{ __('Switch Teams') }}
-                                </div>
-
-                                @foreach (Auth::user()->allTeams() as $team)
-                                    <x-jet-switchable-team :team="$team" />
-                                @endforeach
-
-                                <div class="border-t border-gray-100"></div>
-                            @endif
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-
-                                <x-jet-dropdown-link href="{{ route('logout') }}"
-                                                    onclick="event.preventDefault();
-                                                                this.closest('form').submit();">
-                                    {{ __('Cerrar Sesion') }}
-                                </x-jet-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-jet-dropdown>
-                </div>     
+                            <x-jet-dropdown-link href="{{ route('logout') }}"
+                                                onclick="event.preventDefault();
+                                                            this.closest('form').submit();">
+                                {{ __('Cerrar Sesion') }}
+                            </x-jet-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-jet-dropdown>
             </div>
 
             <!-- Hamburger -->
